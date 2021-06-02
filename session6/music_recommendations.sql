3d6ce30e9ad3f7eb52bc9d71b221aca51e926335
Panos Ipeirotis <ipeirotis@gmail.com>
2021-06-02 13:50:39 -0400

    Update music_recommendations.sql

--- session6/music_recommendations.sql
+++ session6/music_recommendations.sql
@@ -3,15 +3,20 @@ USE facebook;
 # Store the number of people that have liked (any) music
 SET @allmusicfans = (SELECT COUNT(DISTINCT ProfileID) FROM FavoriteMusic);
 
+# The minimum number of fans required, for a band to be analyzed
+# To avoid noise, we keep only bands  that have at least 100 
+# likes in the overall population.
+SET @min_fans = 100;
+
 # Set the band that we are analyzing
-SET @band =  'Jay Z';
+SET @band =  'Bon Jovi';
 
 # Store the number of people that like the specific band
 SET @bandfans = (SELECT COUNT(DISTINCT ProfileID) FROM FavoriteMusic WHERE Music = @band);
 
 # Create a table/view that stores the preferences across all the population
 # We store the band and the percentage of people that liked it
-DROP TABLE IF EXISTS MusicPreferences;
+DROP TEMPORARY TABLE IF EXISTS MusicPreferences;
 CREATE TEMPORARY TABLE MusicPreferences AS
 	SELECT Music, ROUND(COUNT(*)/@allmusicfans,4) AS perc, COUNT(*) AS cnt
 	FROM FavoriteMusic
@@ -20,7 +25,7 @@ CREATE TEMPORARY TABLE MusicPreferences AS
 
 # Create a table with the percentages across only people that like the band 
 # that we specified in the variable @band
-DROP TABLE IF EXISTS BandFans;
+DROP TEMPORARY TABLE IF EXISTS BandFans;
 CREATE TEMPORARY TABLE BandFans AS
 	SELECT Music, ROUND(COUNT(*)/@bandfans,4) AS perc, COUNT(*) AS cnt
 	FROM FavoriteMusic
@@ -35,14 +40,14 @@ CREATE TEMPORARY TABLE BandFans AS
 # Join the two tables above to compare the percentages of likes 
 # in the overall population (T.perc) vs the percentage of likes
 # across the population of people that like the @band (R.perc)
+# We use an OUTER join to keep all the bands from the overall 
+# population, even if they do not appear in the likes of the 
+# fans of the target artist. 
 # We calculate the lift by dividing the two percentages
-# To avoid noise, we keep only bands that have at least 1% likes 
-# in the overall population.
-SELECT R.Music, 
-		R.perc AS perc_focus, R.cnt AS cnt_focus, 
+SELECT T.Music, 
+		COALESCE(R.perc,0.0) AS perc_focus, COALESCE(R.cnt,0) AS cnt_focus, 
         T.perc AS perc_total, T.cnt AS cnt_total,
-        R.perc/T.perc AS lift_ratio
-FROM BandFans R INNER JOIN MusicPreferences T ON R.Music = T.Music
-WHERE T.perc>0.01 AND R.Music != @band
+        COALESCE(R.perc/T.perc,0) AS lift_ratio
+FROM BandFans R RIGHT JOIN MusicPreferences T ON R.Music = T.Music
+WHERE T.cnt>@min_fans AND (R.Music IS NULL OR R.Music != @band)
 ORDER BY lift_ratio DESC
-
